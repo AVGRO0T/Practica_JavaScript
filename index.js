2c9cb37644a3f186bb78e5618b8d711d688fd6ba
nicolas <nicolascaballerobuitrago@gmail.com>
2022-07-25 23:06:46 +0200

    Creating Groups

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+//Random Numbers
+var random = [0,1,2,3,4,5,6,7];
+random = random.sort(function() {return Math.random() - 0.5});
+//Creating groups Playoff
+function groupA(teams){
+    let groupA = [];
+    for (var i = 0 ; i < 2 ; i++){   
+            groupA.push(teams[random[i]])      
+    }
+    return groupA; 
+    
+}
+function groupB(teams){
+    let groupB = [];
+    for (var i = 2 ; i < 4 ; i++){   
+            groupB.push(teams[random[i]])       
+        
+    }
+    return groupB; 
+    
+}
+function groupC(teams){
+    let groupC = [];
+    for (var i = 4 ; i <= 5 ; i++){   
+            groupC.push(teams[random[i]])       
+        
+    }
+    return groupC; 
+    
+}
+function groupD(teams){
+    let groupD = [];
+    for (var i = 6 ; i <= 7 ; i++){   
+            groupD.push(teams[random[i]])       
+        
+    }
+    return groupD; 
+    
+}
+
+
 //Creating 8 teams
 class Teams {
     constructor(name){
@@ -16,4 +57,13 @@ NameTeam.push("Ukraine");
 NameTeam.push("Iceland");
 const NameTeams = new Teams(NameTeam);
 
-console.log(NameTeams.name);
+/* console.log(NameTeams.name); */
+var groupA = groupA(NameTeams.name);
+var groupB = groupB(NameTeams.name);
+var groupC = groupC(NameTeams.name);
+var groupD = groupD(NameTeams.name);
+console.log(groupA);
+console.log(groupB);
+console.log(groupC);
+console.log(groupD);
+
