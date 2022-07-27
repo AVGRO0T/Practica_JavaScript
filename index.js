31d502676546696bece606fca66e3add11a78055
nicolas <nicolascaballerobuitrago@gmail.com>
2022-07-27 19:51:21 +0200

    Modificando estructura

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Creando numeros aleatorios de 0-7 para no repetir el numero.
 var random = [0,1,2,3,4,5,6,7];
 random = random.sort(function() {return Math.random() - 0.5});
-//Creando groups Playoff
+//Creando grupos Playoff
 function groupA(teams){
     let groupA = [];
     for (var i = 0 ; i < 2 ; i++){   
@@ -41,21 +41,26 @@ function groupD(teams){
 
 //Creating 8 teams
 class Teams {
-    constructor(name){
+    constructor(name, goals){
         this.name = name;
         this.players = 11;
+        this.goals = goals;
+    }
+    playofff(){
+        
     }
 }
-const NameTeam = [];
-NameTeam.push("Italy");
-NameTeam.push("Russia");
-NameTeam.push("Spain");
-NameTeam.push("Austria");
-NameTeam.push("England");
-NameTeam.push("Finland");
-NameTeam.push("Ukraine");
-NameTeam.push("Iceland");
-const NameTeams = new Teams(NameTeam);
+//Creando los jugadores
+const Italy = new Teams("Italy") ;
+const Russia = new Teams("Russia") ;
+const Spain = new Teams("Spain") ;
+const Austria = new Teams("Austria") ;
+const England = new Teams("England") ;
+const Finland = new Teams("Finland") ;
+const Ukraine = new Teams("Ukraine") ;
+const Iceland = new Teams("Iceland") ;
+// Poniendo todos los jugadores en un array para repartir en equipos
+let NameTeams = [Italy.name, Russia.name, Spain.name, Austria.name, England.name, Finland.name, Ukraine.name, Iceland.name]
 // Funcion que juega en cuartos de Finales
 function play(groupA,groupB,groupC,groupD){
     const win = true
@@ -69,18 +74,16 @@ function play(groupA,groupB,groupC,groupD){
     groups.push(groupC);
     groups.push(groupD);
 
-    for (let i=0 ; i < 2 ; i++){
-        if 
-    }
+   /* if groups[0][0] == groups  */
 
     return groups[3][1];
 }
 
 /* console.log de la fase de grupos*/
-var groupA = groupA(NameTeams.name);
-var groupB = groupB(NameTeams.name);
-var groupC = groupC(NameTeams.name);
-var groupD = groupD(NameTeams.name);
+var groupA = groupA(NameTeams);
+var groupB = groupB(NameTeams);
+var groupC = groupC(NameTeams);
+var groupD = groupD(NameTeams);
 console.log("==================================================\n== EMPIEZAN LAS FASES ELIMIANTORIAS DEL TORENEO ==\n==================================================\n")
 console.log("Equipos que paticipan en la Playoff:\n")
 console.log(`GRUPO A: ${groupA[0]}, ${groupA[1]}`);
